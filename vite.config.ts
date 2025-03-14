81ac437550a49338676ae24834cfc433e4d3aa7f
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-03-14 12:06:44 +0100

    added nodePolyfills

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    nodePolyfills({ include: ["stream", "events"] }),
+  ],
   build: {
     rollupOptions: {
       input: {
