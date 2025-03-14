a454321cddb9b4abc3041273f29d517361bdea72
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-03-14 15:27:49 +0100

    feat: added NavBar with a title and a select for different accounts

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,62 @@
+import { useQueryClient } from "@tanstack/react-query";
+
+import { Plus } from "lucide-react";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router";
+import { type KeyStorage } from "~/lib/constants/storage";
 import { useStorageLocal } from "~/lib/hooks/useStorageLocal";
+import { spawn } from "~/lib/vault";
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 
 export default function NavBar() {
-  const { data: accounts } = useStorageLocal("accounts");
+  const { accountAddress } = useParams<{ accountAddress: string }>();
+  const { data: accounts } = useStorageLocal<Record<string, KeyStorage>>("accounts");
+
+  const { data: currentAddress } = useStorageLocal<string>("currentAddress");
+
+  useEffect(() => {
+    if (currentAddress && !accountAddress) return;
+
+    void chrome.storage.local.set({ currentAddress: accountAddress });
+  }, [currentAddress, accountAddress]);
+
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  async function handleOnSelect(newValue: "generate" | (string & {})) {
+    if (newValue !== "generate") return navigate(`/wallets/${newValue}`);
+
+    const newAccount = await spawn();
+    queryClient.invalidateQueries({ queryKey: ["local", "accounts"] });
+
+    navigate(`/wallets/${newAccount.address}`);
+  }
 
   return (
-    <div className="flex bg-slate-300 p-2">
-      <h1 className="text-lg font-bold text-slate-900">Dev Wallet</h1>
+    <div className="flex justify-between bg-slate-300 p-2">
+      <h1 className="my-auto text-lg font-bold text-slate-900">Dev Wallet</h1>
 
-      
+      {accountAddress && (
+        <Select value={accountAddress} onValueChange={handleOnSelect}>
+          <SelectTrigger>
+            <SelectValue />
+          </SelectTrigger>
+
+          <SelectContent>
+            {accounts &&
+              Object.entries(accounts).map(([address], i) => (
+                <SelectItem value={address} key={address}>
+                  Account {i + 1}
+                </SelectItem>
+              ))}
+
+            <SelectItem value="generate">
+              Generate <Plus />
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      )}
     </div>
   );
 }
