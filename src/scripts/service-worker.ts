f47444f929186b173093de68c807c58757014cf0
bohdan-sharkz <82876651+bohdan-sharkz@users.noreply.github.com>
2025-03-14 08:38:37 +0100

    feat(): extension 'SIGN' event - added (#1)
    * feat(): extension 'SIGN' event - added
    
    * feat(): patch for jstz_sdk - added
    
    * fix(): service-worker -> type: module - added
    
    * Update src/lib/constants/storage.ts
    
    Co-authored-by: hubert-itsharkz <163825093+hubert-itsharkz@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: bohdan-vasylenko <bohdan.vasylenko@gmail.com>
    Co-authored-by: hubert-itsharkz <163825093+hubert-itsharkz@users.noreply.github.com>

--- src/scripts/service-worker.ts
+++ src/scripts/service-worker.ts
@@ -1,31 +1,76 @@
+import { type AccountStorageType, StorageKeysEnum } from "../lib/constants/storage.ts";
+import { sign } from "../lib/jstz.ts";
+
 enum WalletEvents {
   GET_COUNTER = "GET_COUNTER",
   SET_COUNTER = "SET_COUNTER",
+  SIGN = "SIGN",
 }
 
-interface Data {
-  type: WalletEvents;
+type SignEventDataType = {
+  type: WalletEvents.SIGN;
+  data: {
+    accountAddress: string;
+    operation: unknown;
+  };
+};
+
+interface GenericDataType {
+  type: WalletEvents.GET_COUNTER | WalletEvents.SET_COUNTER;
   data?: unknown;
 }
 
-chrome.runtime.onMessageExternal.addListener(async (request: Data, _sender, sendResponse) => {
-  switch (request.type) {
-    case WalletEvents.GET_COUNTER: {
-      const data = await chrome.storage.local.get("counter");
+chrome.runtime.onMessageExternal.addListener(
+  async (request: GenericDataType | SignEventDataType, _sender, sendResponse) => {
+    switch (request.type) {
+      case WalletEvents.GET_COUNTER: {
+        const data = await chrome.storage.local.get("counter");
 
-      if (!data?.counter) {
-        chrome.storage.local.set({ counter: 0 });
-        data.counter = 0;
+        if (!data?.counter) {
+          chrome.storage.local.set({ counter: 0 });
+          data.counter = 0;
+        }
+
+        sendResponse({ counter: data.counter });
+        break;
       }
 
-      sendResponse({ counter: data.counter });
-      break;
-    }
+      case WalletEvents.SET_COUNTER: {
+        await chrome.storage.local.set({ counter: request.data });
+        sendResponse({ counter: request.data });
+        break;
+      }
 
-    case WalletEvents.SET_COUNTER: {
-      await chrome.storage.local.set({ counter: request.data });
-      sendResponse({ counter: request.data });
-      break;
+      case WalletEvents.SIGN: {
+        const { accountAddress, operation } = request.data ?? {};
+
+        if (!accountAddress) {
+          sendResponse({ error: "Account address is required" });
+          return;
+        }
+
+        const { address: storedAddress } =
+          await chrome.storage.local.get<AccountStorageType>(accountAddress);
+
+        if (!storedAddress) {
+          sendResponse({ error: "Account not found" });
+          return;
+        }
+
+        const {
+          [StorageKeysEnum.ACCOUNT_PUBLIC_KEY]: publicKey,
+          [StorageKeysEnum.ACCOUNT_PRIVATE_KEY]: privateKey,
+        } = storedAddress;
+
+        if (!publicKey || !privateKey) {
+          sendResponse({ error: "No proper public/private keypair found" });
+          return;
+        }
+
+        const signature = sign(operation, privateKey);
+        sendResponse({ signature, publicKey: publicKey });
+        break;
+      }
     }
-  }
-});
+  },
+);
