37f05d09142f8fd8cf045d7d819e66490c7994fc
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-03-14 12:05:17 +0100

    deleted accountAddress from KeyStorage

--- src/lib/constants/storage.ts
+++ src/lib/constants/storage.ts
@@ -1,12 +1,9 @@
-export enum StorageKeysEnum {
+export enum StorageKeys {
   ACCOUNT_PUBLIC_KEY = "account_public_key",
   ACCOUNT_PRIVATE_KEY = "account_private_key",
 }
 
-export type AccountStorageType = Record<
-  string,
-  {
-    [StorageKeysEnum.ACCOUNT_PUBLIC_KEY]?: string | null;
-    [StorageKeysEnum.ACCOUNT_PRIVATE_KEY]?: string | null;
-  }
->;
+export type KeyStorage = {
+  [StorageKeys.ACCOUNT_PUBLIC_KEY]?: string | null;
+  [StorageKeys.ACCOUNT_PRIVATE_KEY]?: string | null;
+};
