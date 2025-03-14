90e5ef039c03473a085ca56b429e04dc2a0c36ce
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-03-14 14:21:42 +0100

    feat: spawn will now add the new wallet to the account property in chrome storage

--- src/lib/vault/index.ts
+++ src/lib/vault/index.ts
@@ -1,6 +1,8 @@
+import * as TaquitoUtils from "@taquito/utils";
+
 import * as Bip39 from "bip39";
 
-import { StorageKeys } from "../constants/storage";
+import { StorageKeys, type KeyStorage } from "../constants/storage";
 import { getPublicKey, seedToHDPrivateKey } from "./misc";
 
 /**
@@ -14,14 +16,26 @@ export async function spawn(mnemonic?: string) {
   const seed = Bip39.mnemonicToSeedSync(mnemonic);
   const privateKey = seedToHDPrivateKey(seed, 0);
   const publicKey = await getPublicKey(privateKey);
+  const accountAddress = TaquitoUtils.getPkhfromPk(publicKey);
 
-  // TODO: Uncomment once we'll be adding support for accounts
-  // const accountAddress = TaquitoUtils.getPkhfromPk(publicKey);
+  const accounts = await getAccounts();
 
-  void chrome.storage.local.set({
+  accounts[accountAddress] = {
     [StorageKeys.ACCOUNT_PUBLIC_KEY]: publicKey,
     [StorageKeys.ACCOUNT_PRIVATE_KEY]: privateKey,
-  });
+  };
 
-  return [publicKey, privateKey];
+  void chrome.storage.local.set({ accounts });
+
+  return { address: accountAddress, publicKey, privateKey };
+}
+
+async function getAccounts(): Promise<Record<string, KeyStorage>> {
+  let { accounts } = await chrome.storage.local.get("accounts");
+  if (!accounts) {
+    chrome.storage.local.set({ accounts: {} });
+    accounts = {};
+  }
+
+  return accounts;
 }
