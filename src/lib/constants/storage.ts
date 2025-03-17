eb21d5b6054572a2c56dd05c053ad91f3f405060
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-03-17 09:52:05 +0100

    feat: deleted unused WalletEventsd

--- src/lib/constants/storage.ts
+++ src/lib/constants/storage.ts
@@ -2,7 +2,7 @@ export enum StorageKeys {
   ACCOUNT_PUBLIC_KEY = "account_public_key",
   ACCOUNT_PRIVATE_KEY = "account_private_key",
   ACCOUNTS = "accounts",
-  CURRENT_ADDRESS = "current_sddress",
+  CURRENT_ADDRESS = "current_address",
 }
 
 export type KeyStorage = Record<StorageKeys | string, string | undefined>;
