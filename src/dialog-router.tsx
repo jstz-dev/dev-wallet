fab317649735b8dd8d52b5c801fd5a1bb0de12f4
bohdan-vasylenko <bohdan.vasylenko@gmail.com>
2025-03-18 10:09:47 +0100

    fix(): popup dialog -> importing wallet - restored

--- src/dialog-router.tsx
+++ src/dialog-router.tsx
@@ -1,21 +1,49 @@
-import { createBrowserRouter } from "react-router";
-import * as Vault from "~/lib/vault";
+import { createBrowserRouter, Navigate, Outlet } from "react-router";
+import type { WalletType } from "~/lib/vault";
+import ImportWallet from "~/pages/ImportWallet.tsx";
+import Wallet from "~/pages/Wallet.tsx";
 
 import { StorageKeys } from "./lib/constants/storage";
-import Home from "./pages/Home";
+import Home, { loader as homeLoader } from "./pages/Home";
 import { WalletEvents } from "./scripts/service-worker";
 
-async function onGenerate() {
-  const newAccount = await Vault.spawnAndSave();
-  await chrome.runtime.sendMessage({ type: WalletEvents.PROCESS_QUEUE, data: newAccount });
+async function onGenerate(payload: WalletType) {
+  await chrome.runtime.sendMessage({ type: WalletEvents.PROCESS_QUEUE, data: payload });
 
-  chrome.storage.local.set({ [StorageKeys.CURRENT_ADDRESS]: newAccount.address });
+  chrome.storage.local.set({ [StorageKeys.CURRENT_ADDRESS]: payload.address });
   window.close();
 }
 
 export const popupRouter = createBrowserRouter([
   {
-    path: "*",
-    element: <Home onGenerate={onGenerate} />,
+    path: "/404",
+    element: <h1>Not found</h1>,
+  },
+  {
+    path: "/index.html",
+    element: <Navigate to="/" />,
+  },
+  {
+    path: "/",
+    element: (
+      <div>
+        <Outlet />
+      </div>
+    ),
+    children: [
+      {
+        index: true,
+        loader: homeLoader,
+        element: <Home onGenerate={onGenerate} />,
+      },
+      {
+        path: "import-wallet",
+        element: <ImportWallet onGenerate={onGenerate} />,
+      },
+      {
+        path: "wallets/:accountAddress",
+        element: <Wallet />,
+      },
+    ],
   },
 ]);
