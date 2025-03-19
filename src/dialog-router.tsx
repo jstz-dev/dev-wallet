a70e3a214dc9e31e0065986da62f4437fec55b26
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-03-19 11:48:16 +0100

    created confirmation prompt when calling the wallet for anything

--- src/dialog-router.tsx
+++ src/dialog-router.tsx
@@ -1,14 +1,18 @@
 import { createBrowserRouter, Navigate, Outlet } from "react-router";
 import type { WalletType } from "~/lib/vault";
-import ImportWallet from "~/pages/ImportWallet.tsx";
-import Wallet from "~/pages/Wallet.tsx";
+import ImportWallet from "~/pages/ImportWallet";
+import Wallet from "~/pages/Wallet";
 
 import { StorageKeys } from "./lib/constants/storage";
+import ConfirmationPrompt from "./pages/ConfirmationPrompt";
 import Home, { loader as homeLoader } from "./pages/Home";
-import { WalletEvents } from "./scripts/service-worker";
+import { WalletRequestEnum } from "./scripts/service-worker";
+
+const params = new URLSearchParams(window.location.search);
+const initialPath = params.get("path");
 
 async function onGenerate(payload: WalletType) {
-  await chrome.runtime.sendMessage({ type: WalletEvents.PROCESS_QUEUE, data: payload });
+  await chrome.runtime.sendMessage({ type: WalletRequestEnum.PROCESS_QUEUE, data: payload });
 
   void chrome.storage.local.set({ [StorageKeys.CURRENT_ADDRESS]: payload.address });
   window.close();
@@ -19,27 +23,31 @@ export const popupRouter = createBrowserRouter([
     path: "/404",
     element: <h1>Not found</h1>,
   },
+
   {
     path: "/index.html",
-    element: <Navigate to="/" />,
+    element: <Navigate to={initialPath ? `/${initialPath}` : "/"} />,
   },
+
   {
     path: "/",
-    element: (
-      <div>
-        <Outlet />
-      </div>
-    ),
     children: [
       {
         index: true,
         loader: homeLoader,
         element: <Home onGenerate={onGenerate} />,
       },
+
+      {
+        path: "confirmation-prompt",
+        element: <ConfirmationPrompt />,
+      },
+
       {
         path: "import-wallet",
         element: <ImportWallet onGenerate={onGenerate} />,
       },
+
       {
         path: "wallets/:accountAddress",
         element: <Wallet />,
