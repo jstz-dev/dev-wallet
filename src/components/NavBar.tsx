c810865d27d37cd361d89ec43ea1ccfe6a9d46f3
Vasylenko <82876651+bohdan-sharkz@users.noreply.github.com>
2025-03-20 13:30:48 +0100

    feat(JSTZ-390): styling - wip (#15)
    Co-authored-by: bohdan-vasylenko <bohdan.vasylenko@gmail.com>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -38,8 +38,9 @@ export default function NavBar() {
   }
 
   return (
-    <div className="flex justify-between bg-slate-300 p-2">
-      <h1 className="my-auto text-lg font-bold text-slate-900">Dev Wallet</h1>
+    <div className="flex justify-between bg-[#5271FF] p-2">
+      <img src={'/jstz_icon.png'} alt={'jstz'} className={'w-10 h-10'} />
+      <h1 className="my-auto text-lg font-bold text-[#000000]">Jstz wallet</h1>
 
       {currentAddress && (
         <Select value={currentAddress} onValueChange={handleOnSelect}>
