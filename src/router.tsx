8c59eea1aefb54b159f40ce246c6899da4dfc208
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-03-20 11:13:00 +0100

    formatting

--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,20 @@
 import { createBrowserRouter, Navigate } from "react-router";
 import RootLayout from "~/layouts/RootLayout";
 import Home, { loader as homeLoader } from "~/pages/Home";
-import Wallet from "~/pages/Wallet";
 import ImportWallet from "~/pages/ImportWallet";
+import Wallet from "~/pages/Wallet";
 
 export const router = createBrowserRouter([
   {
     path: "/404",
     element: <h1>Not found</h1>,
   },
+
   {
     path: "/index.html",
     element: <Navigate to="/" />,
   },
+
   {
     path: "/",
     element: <RootLayout />,
@@ -22,10 +24,12 @@ export const router = createBrowserRouter([
         loader: homeLoader,
         element: <Home />,
       },
+
       {
         path: "import-wallet",
         element: <ImportWallet />,
       },
+
       {
         path: "wallets/:accountAddress",
         element: <Wallet />,
