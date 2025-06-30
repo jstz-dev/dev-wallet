0f302153e3cde91cabe13a987146516992ea327b
bohdan-trili <bohdan.vasylenko@trili.tech>
2025-06-30 15:41:11 +0200

    feat: faucet deployemnt scripts added

--- examples/jstz-faucet/index.ts
+++ examples/jstz-faucet/index.ts
@@ -2,10 +2,10 @@
 const ONE_TEZ = 1000000;
 
 // Maximum amount of tez a requester can receive
-const MAX_TEZ = 1000;
+const MAX_TEZ = ONE_TEZ * 1000;
 
 // Amount of tez to send in one transaction
-const ONE_TIME_AMOUNT = ONE_TEZ * 10
+const ONE_TIME_AMOUNT = ONE_TEZ * 10;
 
 // Get the amount of tez that the smart function has sent to an address
 const getReceivedTez = (requester: Address): number => {
@@ -20,30 +20,18 @@ const setReceivedTez = (requester: Address, received: number): void => {
   Kv.set(`received/${requester}`, received);
 };
 
-// Log the message that the user sent
-const addPoliteMessage = (requester: Address, message: string): void => {
-  let length: number | null = Kv.get(`messages/${requester}/length`);
-  if (length === null) {
-    length = 0;
-  }
-  Kv.set(`messages/${requester}/${length}`, message);
-  Kv.set(`messages/${requester}/length`, length + 1);
-};
-
 // Main function: handle calls to the smart function
 const handler = async (request: Request): Promise<Response> => {
   // Extract the requester's address and message from the request
   const requester = request.headers.get("Referer") as Address;
-  const { message } = await request.json();
 
-  console.log(`${requester} says: ${message}`);
+  console.log(`${requester} requests tez`);
 
   // If the requester already received too much tez, decline the request
   const receivedTez = getReceivedTez(requester);
   if (receivedTez >= MAX_TEZ) {
-    return new Response(
-      JSON.stringify("Sorry, you already received too much tez"),
-    );
+    console.log(`${requester} received ${receivedTez} and is not eligible for more`);
+    return new Response(JSON.stringify("Sorry, you already received too much tez"));
   }
 
   // Process the request and send the 1 tez = 1 million mutez to the requester if you can
@@ -51,20 +39,21 @@ const handler = async (request: Request): Promise<Response> => {
     console.log(
       `Transferring ${ONE_TIME_AMOUNT / ONE_TEZ} tez from ${Ledger.selfAddress} to ${requester}...`,
     );
-    Ledger.transfer(requester, ONE_TIME_AMOUNT);
   } else {
-    return new Response(
-      JSON.stringify("Sorry, I don't have enough tez to fulfill your request"),
+    console.log(
+      `Not enough tez in the faucet to transfer ${ONE_TIME_AMOUNT / ONE_TEZ} tez to ${requester}`,
     );
+    return new Response(JSON.stringify("Sorry, I don't have enough tez to fulfill your request"));
   }
 
   // Log the updates
   setReceivedTez(requester, receivedTez + ONE_TIME_AMOUNT);
-  addPoliteMessage(requester, message);
 
-  return new Response(
-    JSON.stringify(`You received ${ONE_TIME_AMOUNT / ONE_TEZ} tez!`),
-  );
+  return new Response(JSON.stringify(`You received ${ONE_TIME_AMOUNT / ONE_TEZ} tez!`), {
+    headers: {
+      "X-JSTZ-TRANSFER": ONE_TIME_AMOUNT.toFixed(0),
+    },
+  });
 };
 
 export default handler;
---
+++ examples/jstz-faucet/post-deploy.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const path = require('path');
+
+const ENV_FILE_PATH = path.resolve(__dirname, './.env');
+
+// Function to read a value from the .env file
+const getEnvVar = (varName) => {
+  const envFile = fs.readFileSync(ENV_FILE_PATH, 'utf8');
+  const match = envFile.match(new RegExp(`^${varName}=(.*)$`, 'm'));
+  return match ? match[1].replace(/"/g, '') : null;
+};
+
+// Get the environment from the command line arguments
+const env = process.argv[2];
+
+if (!env) {
+  console.error('Usage: node post-deploy.js <dev|staging|prod>');
+  process.exit(1);
+}
+
+// Get the deployed contract address
+const fullAddress = getEnvVar('NEXT_PUBLIC_DEX_BASE_URL');
+
+if (!fullAddress) {
+  console.error('Error: NEXT_PUBLIC_DEX_BASE_URL not found in .env file.');
+  process.exit(1);
+}
+
+const yourAddress = fullAddress.split('://').pop();
+
+console.log(`Topping up address: ${yourAddress}`);
+
+try {
+  if (env === 'dev') {
+    console.log('Topping up for dev environment...');
+    execSync(`jstz bridge deposit --from bootstrap1 --to ${yourAddress} --amount 10000 --network dev`, { stdio: 'inherit' });
+  } else if (env === 'staging') {
+    console.log('Topping up for staging environment...');
+    const topUpUrl = getEnvVar('STAGING_TOP_UP_URL');
+    if (!topUpUrl) {
+      console.error('Error: STAGING_TOP_UP_URL not found in .env file.');
+      process.exit(1);
+    }
+    const command = `curl -XPOST ${topUpUrl} -d '{"from":"bootstrap3","contract":"KT1GFiPkkTjd14oHe6MrBPiRh5djzRkVWcni","amount":5000,"entrypoint":"deposit","arg":"\"${yourAddress}\""}' -H 'Content-Type: application/json'`;
+    execSync(command, { stdio: 'inherit' });
+  } else if (env === 'prod') {
+    console.log('Topping up for prod environment...');
+    const topUpUrl = getEnvVar('PROD_TOP_UP_URL');
+    if (!topUpUrl) {
+      console.error('Error: PROD_TOP_UP_URL not found in .env file.');
+      process.exit(1);
+    }
+    const command = `curl -XPOST ${topUpUrl} -d '{"from":"bootstrap1","contract":"KT1GFiPkkTjd14oHe6MrBPiRh5djzRkVWcni","amount":5000,"entrypoint":"deposit","arg":"\"${yourAddress}\""}' -H 'Content-Type: application/json'`;
+    execSync(command, { stdio: 'inherit' });
+  } else {
+    console.error(`Invalid environment: ${env}. Please use dev, staging, or prod.`);
+    process.exit(1);
+  }
+  console.log(`Top-up complete for ${env} environment.`);
+} catch (error) {
+  console.error(`Error during top-up for ${env} environment:`, error);
+  process.exit(1);
+}
