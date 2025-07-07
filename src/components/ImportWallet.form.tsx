15eb1bd1c791526c3476a6a843aaf0c124f4371c
bohdan-trili <bohdan.vasylenko@trili.tech>
2025-07-07 15:00:40 +0200

    feat(JSTZ-763): import wallet -> "address" and "publicKey" removed and values are deducted from "privateKey" instead

--- src/components/ImportWallet.form.tsx
+++ src/components/ImportWallet.form.tsx
@@ -4,7 +4,7 @@ import { z } from "zod/v4-mini";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { cn } from "~/lib/utils";
-import type { WalletType } from "~/lib/vault";
+import { getAddressAndPublicKey, type WalletType } from "~/lib/vault";
 
 import { PasteButton } from "./PasteButton";
 import { useAppForm } from "./ui/form";
@@ -15,17 +15,15 @@ interface ImportWalletFormProps {
 
 const importWalletSchema = z.object({
   name: z.string().check(z.minLength(1, "Wallet name is required")),
-  address: z.string().check(z.length(36, "Address must be exactly 36 characters long")),
-  publicKey: z.string().check(z.minLength(1,  "Public key is required")),
   privateKey: z.string().check(z.minLength(1, "Private key is required")),
-})
+});
+
+type ImportWalletForm = z.infer<typeof importWalletSchema>;
 
 export function ImportWalletForm({ onSubmit }: ImportWalletFormProps) {
   const form = useAppForm({
     defaultValues: {
       name: "",
-      address: "",
-      publicKey: "",
       privateKey: "",
     },
 
@@ -33,13 +31,27 @@ export function ImportWalletForm({ onSubmit }: ImportWalletFormProps) {
       onSubmit: importWalletSchema,
     },
 
-    onSubmit: ({ value }) => onSubmit(value),
+    onSubmit: async ({ value }) => {
+      const wallet = await generateWallet(value);
+      onSubmit(wallet);
+    },
   });
 
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     e.stopPropagation();
     void form.handleSubmit();
+  }
+
+  async function generateWallet(values: ImportWalletForm) {
+    const { name, privateKey } = values;
+    const { address, publicKey } = await getAddressAndPublicKey(privateKey);
+    return {
+      name,
+      address,
+      publicKey,
+      privateKey,
+    };
   }
 
   return (
@@ -57,77 +69,6 @@ export function ImportWalletForm({ onSubmit }: ImportWalletFormProps) {
                   id="name"
                   type="text"
                   placeholder="Wallet 1"
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                  renderButton={(props) =>
-                    field.state.value.length === 0 ? (
-                      <PasteButton {...props} onPaste={(text) => field.setValue(text)} />
-                    ) : (
-                      <Button
-                        {...props}
-                        variant="ghost"
-                        size="icon_square"
-                        className={cn(props.className, "size-9")}
-                        onClick={() => field.setValue("")}
-                        renderIcon={(props) => <X {...props} />}
-                      />
-                    )
-                  }
-                />
-              </field.FormControl>
-
-              <field.FormMessage />
-            </field.FormItem>
-          )}
-        </form.AppField>
-        <form.AppField name="address">
-          {(field) => (
-            <field.FormItem>
-              <field.FormLabel htmlFor="address" className="text-xs text-white/50 uppercase">
-                Jstz account address:
-              </field.FormLabel>
-
-              <field.FormControl>
-                <Input
-                  id="address"
-                  type="text"
-                  placeholder="Type or paste address"
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                  renderButton={(props) =>
-                    field.state.value.length === 0 ? (
-                      <PasteButton {...props} onPaste={(text) => field.setValue(text)} />
-                    ) : (
-                      <Button
-                        {...props}
-                        variant="ghost"
-                        size="icon_square"
-                        className={cn(props.className, "size-9")}
-                        onClick={() => field.setValue("")}
-                        renderIcon={(props) => <X {...props} />}
-                      />
-                    )
-                  }
-                />
-              </field.FormControl>
-
-              <field.FormMessage />
-            </field.FormItem>
-          )}
-        </form.AppField>
-
-        <form.AppField name="publicKey">
-          {(field) => (
-            <field.FormItem>
-              <field.FormLabel htmlFor="privateKey" className="text-xs text-white/50 uppercase">
-                Public key:
-              </field.FormLabel>
-
-              <field.FormControl>
-                <Input
-                  type="text"
-                  id="privateKey"
-                  placeholder="Type or paste key"
                   value={field.state.value}
                   onChange={(e) => field.handleChange(e.target.value)}
                   renderButton={(props) =>
