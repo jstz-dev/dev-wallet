94b14f0269b1160e4b7d1885b54a0129b282a472
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-10-06 12:56:25 +0200

    replaced @trivago/sort-plugin with prettier-plugin-organize-imports

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,11 @@
 /**
  * @import { Config } from "prettier"
- * @import { PluginConfig } from "@trivago/prettier-plugin-sort-imports"
  * @import { PluginOptions } from "prettier-plugin-tailwindcss"
  */
 
-/** @type {Config & PluginOptions & PluginConfig}  */
+/** @type {Config & PluginOptions}  */
 export default {
-  plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
 
   trailingComma: "all",
   tabWidth: 2,
@@ -14,7 +13,4 @@ export default {
   semi: true,
   singleQuote: false,
   bracketSpacing: true,
-
-  importOrder: ["^@[a-z]+", "<THIRD_PARTY_MODULES>", "^[@/]", "^[./]"],
-  importOrderSeparation: true,
 };
