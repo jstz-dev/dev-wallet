6655ba1ab665aca700bdef685a8b3fe01514c7ef
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-10-10 15:30:52 +0200

    migrated constants

---
+++ apps/signer/src/lib/passkeys/constants.ts
@@ -0,0 +1,7 @@
+/** Represents the "scope" of websites on which a credential should be usable. */
+export const RP_ID = "localhost";
+
+/** The Origin represents the expected URL from which regiration or authentication occurs. */
+export const EXPECTED_ORIGIN = ["http://localhost:4321"];
+
+export const TEN_MINUTES_IN_MS = 60_000;
