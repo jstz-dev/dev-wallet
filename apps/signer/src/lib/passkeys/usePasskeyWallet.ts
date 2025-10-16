197049ae5291d7fd5ad33515b8193e64b62ce673
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-10-16 11:40:44 +0200

    fixed usePasskeyWallet for React Compiler

--- apps/signer/src/lib/passkeys/usePasskeyWallet.ts
+++ apps/signer/src/lib/passkeys/usePasskeyWallet.ts
@@ -1,10 +1,12 @@
-import Wallet from "passkeys-wallet";
 import { useRef } from "react";
+import { PasskeyWallet } from "./PasskeyWallet";
 
 import { userStore } from "./userStore";
 
 export function usePasskeyWallet() {
-  const wallet = useRef(new Wallet(userStore, "localhost", ["http://localhost:4321"], 60_000));
+  const wallet = useRef(
+    new PasskeyWallet(userStore, "localhost", [`chrome-extension://${chrome.runtime.id}`], 60_000),
+  );
 
-  return wallet.current;
+  return wallet;
 }
