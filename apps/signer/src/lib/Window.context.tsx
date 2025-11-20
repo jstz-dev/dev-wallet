ca19b3b3663e48b750d6116427ce3efa395fa4d4
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-11-20 12:02:22 +0100

    fixed exhaustive-deps warning on WindowContextProvider

--- apps/signer/src/lib/Window.context.tsx
+++ apps/signer/src/lib/Window.context.tsx
@@ -1,34 +1,38 @@
-import { createContext, useContext, useState, type PropsWithChildren, useEffect } from "react";
+import { createContext, useContext, useEffect, useRef, type PropsWithChildren } from "react";
 import { ResponseEventTypes } from "~/scripts/service-worker.ts";
 
 interface WindowContext {
-  isProgrammaticallyClosing: boolean;
   close: () => void;
 }
 
 const WindowContext = createContext<WindowContext>({} as WindowContext);
 
 export function WindowContextProvider({ children }: PropsWithChildren) {
-  const [isProgrammaticallyClosing, setIsProgrammaticallyClosing] = useState(false);
+  const isProgrammaticallyClosing = useRef(false);
 
   function onClose() {
-    if (!isProgrammaticallyClosing) {
+    if (!isProgrammaticallyClosing.current) {
       void chrome.runtime.sendMessage({ type: ResponseEventTypes.DECLINE });
     }
+
     window.close();
     return false;
   }
 
   useEffect(() => {
     window.addEventListener("beforeunload", onClose, { once: true });
+
+    return () => {
+      window.removeEventListener("beforeunload", onClose);
+    };
   }, []);
 
   function close() {
-    setIsProgrammaticallyClosing(true);
+    isProgrammaticallyClosing.current = true;
     window.close();
   }
 
-  return <WindowContext value={{ isProgrammaticallyClosing, close }}>{children}</WindowContext>;
+  return <WindowContext value={{ close }}>{children}</WindowContext>;
 }
 
 export function useWindowContext() {
