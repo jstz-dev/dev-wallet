c363f471ea92a28c44b955c30e5489a5e835ba85
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-12-04 14:02:20 +0100

    added theme provider

--- apps/examples/polimarket/src/app/layout.tsx
+++ apps/examples/polimarket/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "~/providers/theme-provider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +24,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+              {children}
+        </ThemeProvider>
       </body>
     </html>
   );
