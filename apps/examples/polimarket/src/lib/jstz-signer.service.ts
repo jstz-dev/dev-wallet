0ac726b9dcc898397a3fa92e68091dd8ad98a8f9
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-12-04 14:07:21 +0100

    migrated useJstzSigner

---
+++ apps/examples/polimarket/src/lib/jstz-signer.service.ts
@@ -0,0 +1,72 @@
+import Jstz, { type ClientOptions } from "@jstz-dev/jstz-client";
+
+import { env } from "~/env";
+
+import * as JstzSigner from "./jstz-signer";
+
+export interface SignWithJstzSignerParams {
+  content:
+    | Jstz.Operation.RunFunction
+    | Jstz.Operation.DeployFunction
+    | Jstz.Operation.RevealLargePayload;
+  signerOptions?: JstzSignerOptions;
+}
+
+export type JstzSignerClientOptions = ClientOptions;
+export interface JstzSignerOptions {
+  timeout?: number;
+}
+
+export function createJstzClient(options?: Partial<JstzSignerClientOptions>) {
+  const { baseURL, timeout = 6000, ...restOptions } = options ?? {};
+  return new Jstz.Jstz({
+    baseURL: baseURL ?? env.NEXT_PUBLIC_JSTZ_NODE_ENDPOINT,
+    timeout: 6000,
+    ...restOptions,
+  });
+}
+
+export async function checkExtensionAvailability() {
+  const jstzSigner = new JstzSigner.JstzSigner(window);
+  return await jstzSigner.callSignerExtension<JstzSigner.CheckStatusResponse>(
+    {
+      type: JstzSigner.SignerRequestEventTypes.CHECK_STATUS,
+    },
+    { timeout: 500 },
+  );
+}
+
+export async function requestAddress() {
+  const jstzSigner = new JstzSigner.JstzSigner(window);
+
+  return await jstzSigner.callSignerExtension<JstzSigner.GetAddressResponse>({
+    type: JstzSigner.SignerRequestEventTypes.GET_ADDRESS,
+  });
+}
+
+export async function signWithJstzSigner({ content, signerOptions }: SignWithJstzSignerParams) {
+  const jstzSigner = new JstzSigner.JstzSigner(window);
+  return await jstzSigner.callSignerExtension<JstzSigner.SignResponse>(
+    {
+      type: JstzSigner.SignerRequestEventTypes.SIGN,
+      content,
+    },
+    signerOptions,
+  );
+}
+
+export async function callSmartFunction({
+  content,
+  onSignatureReceived,
+  options,
+  signerOptions,
+}: SignWithJstzSignerParams & {
+  onSignatureReceived: (
+    response: { data: JstzSigner.SignResponse },
+    jstzClient: Jstz,
+  ) => Promise<void>;
+  options?: JstzSignerClientOptions;
+}) {
+  const request = await signWithJstzSigner({ content, signerOptions });
+  await onSignatureReceived(request, createJstzClient(options));
+}
