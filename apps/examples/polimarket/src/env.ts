81d825ec7863f51b16c2e426bf55aa16cd5951be
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-12-15 07:49:54 +0100

    added PARENT_SF_ADDRESS env

--- apps/examples/polimarket/src/env.ts
+++ apps/examples/polimarket/src/env.ts
@@ -1,6 +1,6 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { vercel } from "@t3-oss/env-nextjs/presets-zod";
-import { z } from "zod";
+import { z } from "zod/mini";
 
 export const env = createEnv({
   extends: [vercel()],
@@ -12,11 +12,13 @@ export const env = createEnv({
   },
 
   client: {
+    NEXT_PUBLIC_PARENT_SF_ADDRESS: z.string().check(z.length(36)),
     NEXT_PUBLIC_API_BASE_URL: z.string(),
     NEXT_PUBLIC_JSTZ_NODE_ENDPOINT: z.string(),
   },
 
   experimental__runtimeEnv: {
+    NEXT_PUBLIC_PARENT_SF_ADDRESS: process.env.NEXT_PUBLIC_PARENT_SF_ADDRESS,
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     NEXT_PUBLIC_JSTZ_NODE_ENDPOINT: process.env.NEXT_PUBLIC_JSTZ_NODE_ENDPOINT,
   },
