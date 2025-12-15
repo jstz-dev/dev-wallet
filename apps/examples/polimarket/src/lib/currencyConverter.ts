07c9d86eab27598a5599900c04d6769aa29513ed
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-12-15 13:33:10 +0100

    added currencyConvert utils

---
+++ apps/examples/polimarket/src/lib/currencyConverter.ts
@@ -0,0 +1,16 @@
+const ONE_TEZ = 1_000_000; // 1 tez = 1 million mutez
+export const TEZ_SYMBOL = "ꜩ";
+
+export function toTez(mutez: number): number {
+  return +(mutez / ONE_TEZ).toFixed(6);
+}
+
+export function toMutez(tez: number): number {
+  return tez * ONE_TEZ;
+}
+
+export function toTezString(mutez?: number | null): string {
+  if (typeof mutez !== "number" || isNaN(mutez)) return "n/a";
+
+  return `${toTez(mutez)} ${TEZ_SYMBOL}`;
+}
