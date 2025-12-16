343d8c70ab3d4ddb7badba5a4ac754645f04e84a
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-12-16 14:42:54 +0100

    added hydration boundry for listing markets

--- apps/examples/polimarket/src/app/(sidebar-layout)/markets/page.tsx
+++ apps/examples/polimarket/src/app/(sidebar-layout)/markets/page.tsx
@@ -1,9 +1,12 @@
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import Link from "next/link";
 import { z } from "zod/mini";
 import { MarketCard } from "~/components/market-card";
 import { env } from "~/env";
 import { createJstzClient } from "~/lib/jstz-signer.service";
 import { marketSchema } from "~/lib/validators/market";
+import { getQueryClient } from "~/providers/query-provider";
+import { smartFunctions } from "~/queries/smartFunctions.queries";
 
 const marketFromRoot = z.object({
   markets: z.record(
@@ -15,20 +18,25 @@ const marketFromRoot = z.object({
 });
 
 export default async function MarketsPage() {
+  const queryClient = getQueryClient();
   const jstzClient = createJstzClient();
-  const kv = await jstzClient.accounts
-    .getKv(env.NEXT_PUBLIC_PARENT_SF_ADDRESS, { key: "root" })
-    .then((value): unknown => JSON.parse(value));
 
-  const { data: marketsFromRoot, error } = marketFromRoot.safeParse(kv);
+  const options = smartFunctions.getKv(env.NEXT_PUBLIC_PARENT_SF_ADDRESS, "root", jstzClient);
+
+  await queryClient.prefetchQuery(options);
+
+  const kv = await queryClient.getQueryData(options.queryKey);
+  const { data: marketsFromRoot, error } = marketFromRoot.safeParse(JSON.parse(kv));
   if (error) {
     throw error;
   }
 
   const markets = await Promise.all(
     Object.values(marketsFromRoot.markets).map(async ({ address }) => {
-      const kv = await jstzClient.accounts.getKv(address, { key: "root" });
+      const options = smartFunctions.getKv(address, "root", jstzClient);
+      await queryClient.prefetchQuery(options);
 
+      const kv = await queryClient.getQueryData(options.queryKey);
       return { ...marketSchema.parse(JSON.parse(kv)), address };
     }),
   );
@@ -46,11 +54,13 @@ export default async function MarketsPage() {
         </div>
 
         <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-          {markets.map((market) => (
-            <Link href={`/markets/${market.address}`} key={market.address}>
-              <MarketCard {...market} />
-            </Link>
-          ))}
+          <HydrationBoundary state={dehydrate(queryClient)}>
+            {markets.map((market) => (
+              <Link href={`/markets/${market.address}`} key={market.address}>
+                <MarketCard {...market} />
+              </Link>
+            ))}
+          </HydrationBoundary>
         </div>
       </div>
     </main>
