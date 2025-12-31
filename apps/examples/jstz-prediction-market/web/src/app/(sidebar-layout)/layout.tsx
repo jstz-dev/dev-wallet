a77c5e9e3951e155cd825e925b4f8036251c81f4
Hubert Kowalski <hubert@itsharkz.com>
2025-12-31 13:04:09 +0100

    added logout button and ProtectedLayout to SidebarLayout

--- apps/examples/jstz-prediction-market/web/src/app/(sidebar-layout)/layout.tsx
+++ apps/examples/jstz-prediction-market/web/src/app/(sidebar-layout)/layout.tsx
@@ -1,10 +1,36 @@
+import { LogOut } from "lucide-react";
+import Form from "next/form";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import NavigationBar from "~/components/navigation-bar";
+import { SidebarMenuButton } from "~/components/ui/sidebar";
+import { auth } from "~/lib/auth";
+import ProtectedLayout from "~/lib/auth/protected-layout";
 
 export default function SidebarLayout({ children }: LayoutProps<"/">) {
+  async function signOut() {
+    "use server";
+    const { success } = await auth.api.signOut({ headers: await headers() });
+
+    if (success) {
+      redirect("/login");
+    }
+  }
+
   return (
-    <div className="flex min-h-screen w-full">
-      <NavigationBar />
-      {children}
-    </div>
+    <ProtectedLayout>
+      <div className="flex min-h-screen w-full">
+        <NavigationBar>
+          <Form action={signOut}>
+            <SidebarMenuButton className="cursor-pointer">
+              <LogOut />
+              Logout
+            </SidebarMenuButton>
+          </Form>
+        </NavigationBar>
+
+        {children}
+      </div>
+    </ProtectedLayout>
   );
 }
