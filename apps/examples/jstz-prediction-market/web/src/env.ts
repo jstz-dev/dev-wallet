d391b7abb39b538976a1cf94ac26aa8096c8c52f
Hubert Kowalski <hubert@itsharkz.com>
2025-12-31 13:02:28 +0100

    added env variables to env.ts

--- apps/examples/jstz-prediction-market/web/src/env.ts
+++ apps/examples/jstz-prediction-market/web/src/env.ts
@@ -6,18 +6,36 @@ export const env = createEnv({
   extends: [vercel()],
 
   server: {
+    ALLOWED_DOMAINS: z.pipe(
+      z.string(),
+      z.transform((val) => val.split(",")),
+    ),
+    ALLOWED_EMAILS: z.pipe(
+      z.string(),
+      z.transform((val) => val.split(",")),
+    ),
+
+    AUTH_SECRET: z.string(),
+
+    GOOGLE_AUTH_SECRET: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+
+    DB_URL: z.string(),
+
     WALLET_ADDRESS: z.string(),
     WALLET_PUBLIC_KEY: z.string(),
     WALLET_SECRET_KEY: z.string(),
   },
 
   client: {
+    NEXT_PUBLIC_AUTH_URL: z.url(),
     NEXT_PUBLIC_PARENT_SF_ADDRESS: z.string().check(z.length(36)),
     NEXT_PUBLIC_API_BASE_URL: z.string(),
     NEXT_PUBLIC_JSTZ_NODE_ENDPOINT: z.string(),
   },
 
   experimental__runtimeEnv: {
+    NEXT_PUBLIC_AUTH_URL: process.env.NEXT_PUBLIC_AUTH_URL,
     NEXT_PUBLIC_PARENT_SF_ADDRESS: process.env.NEXT_PUBLIC_PARENT_SF_ADDRESS,
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     NEXT_PUBLIC_JSTZ_NODE_ENDPOINT: process.env.NEXT_PUBLIC_JSTZ_NODE_ENDPOINT,
